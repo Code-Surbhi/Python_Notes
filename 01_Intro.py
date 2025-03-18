7f75d56ecf32b3148a54132bd4c442c40d78f941
Surbhi Singh <code.surbhi1712@gmail.com>
2025-03-18 10:24:32 +0530

    Create 01_Intro.py

---
+++ 01_Intro.py
@@ -0,0 +1,96 @@
+'''
+    Low level Language -> Machine Language and assembly language
+    High level Language -> General Purpose, Special Purpose, Scripting Language
+
+    General Purpose language -> Strict rules and syntax , C, C++, C# , Java , Python
+    Special Purpose language -> WebDev , MobileDev , Developing Games , android , ios
+    Scripting -> Easier to write and understand and learn
+
+    HLL-> 
+        1. Compiler based -> c, c++
+        2. Interpreter based -> php , js
+        3. Hybrid -> python , java , c#.net
+
+    Compiler -> 
+        Sorce code should not have any error
+        Compiler is faster than interpreter -> complete machine code is made , translation done only once '
+
+    Interpreter->
+        Interpreter languages are more flexible as they provide runtime environment (Browser provides a R.E for js)
+        Seperate file is note made here
+        Translation is done everytime it runs
+        Line by line translate and execute and then it moves to the next line 
+    
+    Hybrid ->
+    Source Code(program.py) --compiler--> Byte Code(program.pyc) --interpreter--> Code runs
+    program.pyc -> error free because the compiler checks if it has no error
+    PVM -> python virtual machine -> Runtime environment of python
+    Interpreter -> Line by line + aek sath -> because of pyc -> done by JIT compiler (just in time)
+
+    Python -> Platform independent and portable
+        sourceCode (program.py) --compiler--> bytecode (program.pyc) -> this pyc file can run on mac machine , linux , windows , everywhere (because of PVM)
+        PVM -> every OS will have its own version of PVM
+    
+        on the contrary -> 
+        First.c --compiler--> first.exe -> windows pe run hojayega but not on linux -> it will have to be recompiled here 
+        First.js --interpreter--> Windows and linux (it will run on both) -> interpreter based languages are also platform independent
+        
+'''
+'''
+    Program -> Data and Instructions
+
+    Paradigm / Style of programming ->
+        1. Funtional / Procedural Programming -> function specific
+            Funtion -> a set of instructions/code that performs specific tasks
+            fun1() fun2() fun3() -> data1
+
+        2.Object Oriented Programming -> 
+            Data Centric and in OOPs we create objects of these classes and use them 
+
+            Class A -> fun1() fun2() fun3() -> data1
+            Class B -> f1() f2() f3()
+
+        3.Modular Programming -> broken down into modules
+            Date and time Module -> Class Date , Class TIme , funToday()
+            Employee Module -> Class Module , Class Attendace , funSalary()
+'''
+'''
+    Features of Python ->
+    1. Easy to learn
+    2. Simple/Expressive
+    3.Scripting Language
+    4.Open source
+    5. General purpose
+    6. Interpreter Langauge(Hybrid)
+    7. Platform Independent
+    8. Portable
+    9. Dynamically types
+    10. Object Oriented and procedural
+    11. Extensive libraries
+    12.Extensible -> c,c++ ke sath bhi hum use kar sakte hai
+    13.Embeddable -> java mai use ho sakta hai
+    14.Scalable
+'''
+'''
+    Areas of Programming 
+
+    Programming language -> Web Programming
+                            GUI programming 
+                            Data science
+                            AI
+                            ML
+                            Mobile Application
+                            Multithread programming
+                            Game development
+                            Network programming
+                            Enterprise application
+                            Database programming
+    
+    1.Learn about core python 
+    2.Then choose the interested area -> e.g learn about AI
+    3.Then learn the library provided by that language
+'''
+'''
+    Python cmd -> to exit -> ctrl+z
+    Python --version
+'''
